a85f5c7788e84c0d26b91ef0218aa31bbf417bbe
shpolina <polina@careevolution.com>
2017-08-08 20:22:08 -0400

    Update announcement

--- index.php
+++ index.php
@@ -6,19 +6,15 @@ include "header.php";
         <div class="top-wrapper">
 
             <div class="announce-text-block col-lg-5 col-md-5 col-sm-5 col-xs-8">
-                <a href="dolls-house.php" class="btn-link btn-s announcement-btn" id="announcent-clickable-section">
-                    <h3 class="coming-next">Coming Soon!</h3>
-                    <h3 class="announcement-dates">April 4th 2017</h3>
-                    <h3 class="show-name">Proposal</h3>
-                    <h3 class="show-description"> One Act Comedy by Anton Chekhov
-
-                        A FREE performance followed by lecture on Russian politics
-
-                        Read More...</h3>
+                <a href="" class="btn-link btn-s announcement-btn" id="announcent-clickable-section">
+                    <h3 class="coming-next">Coming Soon:</h3>
+                    <h3 class="announcement-dates">2017-2018 Season Announcement</h3>
+                <!--    <h3 class="show-name">Proposal</h3>-->
+                    <h3 class="show-description">We will be announcing our shows and auditions shortly</h3>
                 </a>
-                <a href="http://tickets.thechambertheatre.com/" target="_blank"
+            <!--    <a href="http://tickets.thechambertheatre.com/" target="_blank"
                    class="btn btn-buy-tickets-top hover-decoration" role="button">BUY
-                    TICKETS</a>
+                    TICKETS</a>-->
             </div>
             <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
                 <img src="img/top_image_homepage.jpg" class="home-page-top-img">

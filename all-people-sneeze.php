c9d77fd9998dcfa8ebc86b03a42de773a6210bf9
shpolina <polina@careevolution.com>
2017-08-18 22:15:15 -0400

    add all people sneeze

---
+++ all-people-sneeze.php
@@ -0,0 +1,260 @@
+<?php
+include "header.php";
+?>
+    <div class="fill-background-black">
+        <div class="container-fluid announce-container">
+            <div class="top-wrapper">
+
+               <div class="announce-text-block col-lg-5 col-md-5 col-sm-5 col-xs-8">
+                    <a href="tales-from-the-hills.php" class="btn-link btn-s announcement-btn" id="announcent-clickable-section">
+                        <h3 class="coming-next">Coming Next:</h3>
+                        <h3 class="announcement-dates">11.10.17 - 11.18.17</h3>
+                        <h3 class="show-name">TALES FROM THE HILLS</h3>
+                        <h3 class="show-description">WB Yeats’ verse tragedy Deirdre and <br> J.M. Synge’s raucous comedy In The Shadow of The Glen</h3>
+                </a>
+                <!--    <a href="http://tickets.thechambertheatre.com/" target="_blank"
+                   class="btn btn-buy-tickets-top hover-decoration" role="button">BUY
+                    TICKETS</a>-->
+                <a href="https://www.facebook.com/events/1907604012834766/" target="_blank"
+                   class="btn btn-buy-tickets-top hover-decoration" role="button">AUDITIONS</a>
+                </div>
+            <!--    <div class="announce-text-block col-lg-5 col-md-5 col-sm-5 col-xs-8">
+                    <div class="btn-link btn-s announcement-btn" id="announcent-clickable-section">
+                        <h3 class="coming-next">Coming Soon:</h3>
+                        <h3 class="announcement-dates">2017-2018 Season Announcement</h3>
+                        <h3 class="show-description">We will be announcing our shows and auditions shortly</h3>
+                    </div>
+                </div>-->
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
+                    <img src="img/3_3.jpg" class="home-page-top-img">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-5 col-lg-offset-1 col-md-6 col-sm-6 col-xs-12 panel panel-default chek-mate-slider">
+                <div class="panel-heading">
+                    <div class="homepage-heading-style">All People Sneeze</div>
+                </div>
+                <div class="panel-body">
+                    <p>
+                        <span class="show-subheading-style">From short stories by Anton Chekhov</span><br> 
+                        When it is time to see what the humanity is become, how will be know it?  Will we know it by how we behave with our spouses?  
+                        How we treat the dogs of others?  How we cope with jealousy?  So many ways to approach these dilemmas, so many stories of how.  
+                        Perhaps there is one thing that unites all people - the need to tell their stories.  That, and the fact that all people sneeze. <br>
+                        Rifling through the pages of Chekhov's short stories, we look in on people of all backgrounds as they the get on with the daily business 
+                        of eating, drinking, living, and dying, with Chekhov as our guide through their world.
+                    </p>
+                    <p>
+                        <a href="https://www.google.com/maps/place/Hope+Culture+Church/@38.2625772,-85.7151737,17z/data=!4m12!1m6!3m5!1s0x88697317a7fc6239:0xcf1ffbe866bb3273!2sHope+Culture+Church!8m2!3d38.262573!4d-85.712985!3m4!1s0x88697317a7fc6239:0xcf1ffbe866bb3273!8m2!3d38.262573!4d-85.712985" 
+                            target="_blank" 
+                            class="btn hope-culture-btn">HOPE COMMUNITY & COFFEE 
+                            <i class="fa fa-map-marker"></i> </a> | <a href="https://www.facebook.com/events/1228137350541427/" 
+                                                                        target="_blank" 
+                                                                        class="btn vault-btn">
+                                                                                March 2, 3, 5, 8, 9, 10 at 7:30pm</a>
+                    </p>
+                </div>
+
+             <!--   <div class="show-slider panel-body">
+                    <div class="show-subheading-style">PHOTOS FROM THE SHOW</div>
+
+                    <section class="chek-mate-photos-section">
+                        <div class="chek-mate-photos-container">
+                            <div class="carousel slide article-slide gallery-carousel" id="article-photo-carousel">
+                                <div class="carousel-inner cont-slider">
+                                    <div class="item active">
+                                        <img alt="" title="" src="img/creditors-photos/14.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/1.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/2.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/3.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/4.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/5.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/6.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/7.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/8.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/9.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/10.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/11.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/12.jpg">
+                                    </div>
+                                    <div class="item">
+                                        <img alt="" title="" src="img/creditors-photos/13.jpg">
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <ol class="gallery-carousel-indicators carousel-indicators">
+                                        <li class="active" data-slide-to="0" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/14.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="1" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/1.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="2" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/2.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="3" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/3.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="4" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/4.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="5" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/5.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="6" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/6.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="7" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/7.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="8" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/8.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="9" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/9.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="10" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/10.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="11" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/11.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="12" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/12.jpg">
+                                        </li>
+                                        <li class="" data-slide-to="13" data-target="#article-photo-carousel">
+                                            <img alt="" src="img/creditors-photos/13.jpg">
+                                        </li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>  -->
+
+            <!--    <div class="show-video panel-body">
+                    <div class="show-subheading-style">WATCH VIDEO FROM THE SHOW</div>
+                    <div class="embed-responsive embed-responsive-16by9 list-group-item">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/HLF7pEoOjIM"
+                                allowfullscreen></iframe>
+                    </div>
+                </div>-->
+            </div>
+
+            <div class="col-lg-5 col-md-6 col-sm-6 col-xs-12 panel panel-default chek-mate-crew">
+                <div class="panel-body">
+                    <div class="col-md-6">
+
+                        <div class="crew">
+                            <span class="gold-heading-small text-weight-400"> Director </span><br>
+                            <span class="text-weight-400">Martin French  </span>
+                        </div>
+
+                   <!--     <div class="crew">
+                            <span class="gold-heading-small text-weight-400"> Assistant Director </span><br>
+                            <span class="text-weight-400">Jay Marie Padilla</span>
+                        </div>
+
+                        <div class="crew">
+                            <span class="gold-heading-small text-weight-400"> Stage Manager </span><br>
+                            <span class="text-weight-400">Clare Hagan</span>
+                        </div>
+
+                        <div class="crew">
+                            <span class="gold-heading-small text-weight-400"> Design </span><br>
+                            <span class="text-weight-400">Martin French, Clare Hagan, Polina Shafran</span>
+                        </div>-->
+
+
+                    </div>
+                    <div class="col-md-6">
+                        <div class="crew">
+                            <span class="gold-heading-small text-weight-400"> Poster Design </span><br>
+                            <span class="text-weight-400">Elina Unger</span>
+                        </div>
+                  <!--      <div class="crew">
+                            <span class="gold-heading-small text-weight-400">Cast </span><br>
+                            <span class="text-weight-400">Michael Drury </span>
+                            <span class="text-weight-400">Ryan Watson </span>
+                            <span class="text-weight-400">Polina Shafran</span>
+                        </div>-->
+                    </div>
+
+               <!--     <div class="golden-quote col-md-12">
+                        <a href="http://arts-louisville.com/2016/12/03/threes-company/">
+                            <p class="quote-text">
+                                <i class="unicode-quote">&#x201c</i>
+                                To watch the dark, revelatory heart of
+                                Creditors play out in the hands of these three is fascinating, and as lean
+                                and economical a piece of theatre as you are likely to find.
+                                <br>
+                                <span class="quote-author">Keith Waits, Arts-Lousiville</span>
+                            </p>
+                        </a>
+
+
+                        <a href="http://www.broadwayworld.com/louisville/article/BWW-Review-CREDITORS-Presented-by-The-Chamber-Theatre-at-Vault-1031-20161205">
+                            <p class="quote-text">
+                                <i class="unicode-quote">&#x201c</i>
+                                It's deep and insanely thought provoking,
+                                with performances that leave your jaw practically attached to the floor.
+                                <br>
+                                <span class="quote-author">Taylor Clemons, BWW</span>
+                            </p>
+                        </a>
+
+                        <a href="http://www.leoweekly.com/2016/12/creditors-action-way-weve-never-experienced/">
+                            <p class="quote-text">
+                                <i class="unicode-quote">&#x201c</i>
+                                It’s a unique setup, risky and strange,
+                                that brings us into the action in a way I’ve never before experienced — and
+                                it calls for extraordinary acting.
+                                <br>
+                                <span class="quote-author">Marty Rosen, LEO Weekly</span>
+                            </p>
+                        </a>
+
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!--<div class="fill-background-creditors-poster">
+    <div class="container-fluid">
+        <img src="img/creditors-flyer.jpg" class="img-responsive creditors-img" alt="1">
+    </div> -->
+
+<?php
+include "footer.php";
+?>
